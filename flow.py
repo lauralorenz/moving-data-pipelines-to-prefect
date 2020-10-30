7e45e9cab4d6b2b983563cbb972c893f87f6d98b
Prefect <hooloovoo@Lauras-MacBook-Pro.local>
2020-10-30 12:20:40 -0400

    start to take extract out as its own function

--- flow.py
+++ flow.py
@@ -3,17 +3,12 @@ from prefect.environments.storage import Docker
 from prefect.triggers import always_run
 
 @task
-def flow_pieces(db_name):
+def flow_pieces(filenames, db_name):
 	import sys
 	import os
 
-	from python.manipulation import transform_features, save_to_db, extract
+	from python.manipulation import transform_features, save_to_db
 	from python.machine_learning import its_a_linear_regression
-
-
-	filenames = []
-	for i in [1,2,3]:
-		filenames.append(extract("https://next.json-generator.com/api/json/get/Vy7eluVOK", i))
 
 	for filename in filenames:
 		input_file = os.path.abspath(filename)
@@ -22,9 +17,19 @@ def flow_pieces(db_name):
 		its_a_linear_regression(df, output_file)
 		save_to_db(db_name, output_file)
 
+@task
+def extract_filenames():
+	from python.manipulation import extract
+	filenames = []
+	for i in [1,2,3]:
+		filenames.append(extract("https://next.json-generator.com/api/json/get/Vy7eluVOK", i))
+	return filenames
+
+
 with Flow("shiny new flow", storage=Docker(base_image="spookyimage-shiny", local_image=True)) as f:
 	db_name = Parameter('db_name', default="scary_model_storage.db")
-	flow_pieces(db_name)
+	filenames = extract_filenames()
+	flow_pieces(filenames, db_name)
 
 
 ## run flow and print logs
